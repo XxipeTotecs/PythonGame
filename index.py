d513a5778ba5e1d586bb4eeab00cd0da80d0ac50
Eduardo Vinicius <dudugrifin22@gmail.com>
2023-10-10 21:45:32 -0300

    adicionando o score

--- index.py
+++ index.py
@@ -1,17 +1,22 @@
 import pygame
 from sys import exit
 
+
 pygame.init()
 screen = pygame.display.set_mode((800, 400))
 pygame.display.set_caption('O Corredor')
 clock = pygame.time.Clock()
 test_font = pygame.font.Font('UltimatePygameIntro/font/Pixeltype.ttf', 40)
 
+
 sky_surface = pygame.image.load(
     'UltimatePygameIntro/graphics/Sky.png').convert()
 ground_surface = pygame.image.load(
     'UltimatePygameIntro/graphics/ground.png').convert()
-text_surface = test_font.render('Bem Vindo ao Corredor', False, 'Black')
+
+
+score_surface = test_font.render('Bem Vindo ao Corredor', False, 'Black')
+score_retangulo = score_surface.get_rect(center=(400, 50))
 
 snail_surface = pygame.image.load(
     'UltimatePygameIntro/graphics/snail/snail1.png').convert_alpha()
@@ -21,17 +26,18 @@ player_surface = pygame.image.load(
     'UltimatePygameIntro/graphics/Player/player_walk_1.png').convert_alpha()
 player_retangulo = player_surface.get_rect(midbottom=(80, 300))
 
+
 while True:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             pygame.quit()
             exit()
+        # if event.type == pygame.MOUSEMOTION:
+        #    if player_retangulo.collidepoint(event.pos)
 
-    # escrever todos nossos elementos
-    # dar update em tudo
     screen.blit(sky_surface, (0, 0))
     screen.blit(ground_surface, (0, 300))
-    screen.blit(text_surface, (250, 50))
+    screen.blit(score_surface, score_retangulo)
 
     snail_retangulo.x -= 4
     if snail_retangulo.right <= 0:
