82ed88868d7c1a00ce4f09c6b9c3b30900d85433
Eduardo Vinicius <dudugrifin22@gmail.com>
2023-10-11 20:03:45 -0300

    atualizando as cores

--- index.py
+++ index.py
@@ -15,7 +15,7 @@ ground_surface = pygame.image.load(
     'UltimatePygameIntro/graphics/ground.png').convert()
 
 
-score_surface = test_font.render('Bem Vindo ao Corredor', False, 'Black')
+score_surface = test_font.render('Bem Vindo ao Corredor', False, (64, 64, 64))
 score_retangulo = score_surface.get_rect(center=(400, 50))
 
 snail_surface = pygame.image.load(
@@ -33,10 +33,12 @@ while True:
             pygame.quit()
             exit()
         # if event.type == pygame.MOUSEMOTION:
-        #    if player_retangulo.collidepoint(event.pos)
+        # if player_retangulo.collidepoint(event.pos)
 
     screen.blit(sky_surface, (0, 0))
     screen.blit(ground_surface, (0, 300))
+    pygame.draw.rect(screen, '#c0e8ec', score_retangulo)
+    pygame.draw.rect(screen, '#c0e8ec', score_retangulo, 10)
     screen.blit(score_surface, score_retangulo)
 
     snail_retangulo.x -= 4
