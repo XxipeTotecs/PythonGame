a72162540a0ea09610f4386e099575c32b635303
Eduardo Vinicius <dudugrifin22@gmail.com>
2023-10-13 22:23:44 -0300

    adicionando tempo

--- index.py
+++ index.py
@@ -2,12 +2,20 @@ import pygame
 from sys import exit
 
 
+def displayScore():
+    current_time = pygame.time.get_ticks() - start_game
+    score_surface = test_font.render(f'{current_time}', False, (64, 64, 64))
+    score_retangulo = score_surface.get_rect(center=(400, 50))
+    screen.blit(score_surface, score_retangulo)
+
+
 pygame.init()
 screen = pygame.display.set_mode((800, 400))
 pygame.display.set_caption('O Corredor')
 clock = pygame.time.Clock()
 test_font = pygame.font.Font('UltimatePygameIntro/font/Pixeltype.ttf', 40)
 game_active = True
+start_game = 0
 
 sky_surface = pygame.image.load(
     'UltimatePygameIntro/graphics/Sky.png').convert()
@@ -15,8 +23,8 @@ ground_surface = pygame.image.load(
     'UltimatePygameIntro/graphics/ground.png').convert()
 
 
-score_surface = test_font.render('Bem Vindo ao Corredor', False, (64, 64, 64))
-score_retangulo = score_surface.get_rect(center=(400, 50))
+# score_surface = test_font.render('Bem Vindo ao Corredor', False, (64, 64, 64))
+# score_retangulo = score_surface.get_rect(center=(400, 50))
 
 snail_surface = pygame.image.load(
     'UltimatePygameIntro/graphics/snail/snail1.png').convert_alpha()
@@ -46,13 +54,15 @@ while True:
             if event.type == pygame.KEYDOWN and event.key == pygame.K_SPACE:
                 game_active = True
                 snail_retangulo.left = 800
+                start_game = pygame.time.get_ticks()
 
     if game_active:
         screen.blit(sky_surface, (0, 0))
         screen.blit(ground_surface, (0, 300))
-        pygame.draw.rect(screen, '#c0e8ec', score_retangulo)
-        pygame.draw.rect(screen, '#c0e8ec', score_retangulo, 10)
-        screen.blit(score_surface, score_retangulo)
+        # pygame.draw.rect(screen, '#c0e8ec', score_retangulo)
+        # pygame.draw.rect(screen, '#c0e8ec', score_retangulo, 10)
+        # screen.blit(score_surface, score_retangulo)
+        displayScore()
 
         snail_retangulo.x -= 4
         if snail_retangulo.right <= 0:
