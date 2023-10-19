e25dcd44d09c99de1c5a8d37268e45941a9050e3
Eduardo Vinicius <dudugrifin22@gmail.com>
2023-10-19 19:51:01 -0300

    OOP

--- index.py
+++ index.py
@@ -3,6 +3,49 @@ from sys import exit
 from random import randint
 
 
+class Player(pygame.sprite.Sprite):
+    def __init__(self):
+        super().__init__()
+
+        player_walk_1 = pygame.image.load(
+            'UltimatePygameIntro/graphics/Player/player_walk_1.png').convert_alpha()
+        player_walk_2 = pygame.image.load(
+            'UltimatePygameIntro/graphics/Player/player_walk_2.png').convert_alpha()
+        self.player_walk = [player_walk_1, player_walk_2]
+        self.player_index = 0
+        self.player_jump = pygame.image.load(
+            'UltimatePygameIntro/graphics/Player/jump.png').convert_alpha()
+
+        self.image = self.player_walk[self.player_index]
+        self.rect = self.image.get_rect(midbottom=(200, 300))
+        self.gravity = 0
+
+    def player_input(self):
+        keys = pygame.key.get_pressed()
+        if keys[pygame.K_SPACE] and self.rect.bottom >= 300:
+            self.gravity = -20
+
+    def apply_gravity(self):
+        self.gravity += 1
+        self.rect.y += self.gravity
+        if self.rect.bottom >= 300:
+            self.rect.bottom = 300
+
+    def animation_state(self):
+        if self.rect.bottom < 300:
+            self.image = self.player_jump
+        else:
+            self.player_index += 0.1
+            if self.player_index >= len(self.player_walk):
+                self.player_index = 0
+            self.image = self.player_walk[int(self.player_index)]
+
+    def update(self):
+        self.player_input()
+        self.apply_gravity()
+        self.animation_state()
+
+
 def displayScore():
     current_time = int(pygame.time.get_ticks() / 1000) - start_game
     score_surface = test_font.render(
@@ -60,6 +103,9 @@ test_font = pygame.font.Font('UltimatePygameIntro/font/Pixeltype.ttf', 40)
 game_active = False
 start_game = 0
 score = 0
+
+player = pygame.sprite.GroupSingle()
+player.add(Player())
 
 sky_surface = pygame.image.load(
     'UltimatePygameIntro/graphics/Sky.png').convert()
@@ -191,6 +237,8 @@ while True:
             player_retangulo.bottom = 300
             player_animation()
         screen.blit(player_surface, player_retangulo)
+        player.draw(screen)
+        player.update()
 
         # Obstacle Movement
         obstacle_rect_list = obstacleMovement(obstacle_rect_list)
